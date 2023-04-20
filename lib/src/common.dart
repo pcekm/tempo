b91a3e70110482e67020a8b90e04cbc403dbb083
Pat Ekman <pat@ekman.cx>
2023-04-19 21:15:56 -0700

    Checked in missed common.dart.

---
+++ lib/src/common.dart
@@ -0,0 +1,27 @@
+/// Common constants and functions for date time handling.
+
+const List<int> _daysInMonthTable = [
+  0,
+  31, // Jan
+  29, // Feb; non-leap years are a special case
+  31, // Mar
+  30, // Apr
+  31, // May
+  30, // Jun
+  31, // Jul
+  31, // Aug
+  30, // Sep
+  31, // Oct
+  30, // Nov
+  31, // Dec
+];
+
+/// Determines if a year is a leap year.
+bool checkLeapYear(int year) =>
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+
+/// Returns the days in a given month.
+int daysInMonth(int year, int month) {
+  return _daysInMonthTable[month] -
+      (month == 2 && !checkLeapYear(year) ? 1 : 0);
+}
